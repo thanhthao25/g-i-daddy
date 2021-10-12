e6535cf7065ecd1cc0e28c4287f73b9a4bb16e18
thanhthao25 <92370572+thanhthao25@users.noreply.github.com>
2021-10-12 21:57:03 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Daddy oiii!",
+  text2: "Em có điều này muốn hỏi anh nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Anh iu bé có phải không nào ._.",
+  text4: "Nếu anh ko trả lời mà thoát ra tức là muốn làm người iu rùi đó nha :v",
+  text5: "Em mơ à???",
+  text6: "Iu ơi là iu <3",
+  text7: "lí do anh thích bé đi :vvvv",
+  text8: "Gửi cho bé <3",
+  text9: "Vì bé đáng iu muốn xỉuuuuu",
+  text10: "Bé biết mà ^^ Iu anh moaaa",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Daddy thấy bé giỏi homm :v Nếu anh nhớ em thì nhắn tin cho em đi ><",
+  text12: "Dẹeee <3",
 };
 
 $(document).ready(function () {
